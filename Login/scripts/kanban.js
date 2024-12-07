9b6ac198ca454348a992ed2c00dbfe0e78ead0df
Nascido <rafael124715@gmail.com>
2024-12-07 12:54:08 -0300

    Editar tarefas

--- Login/scripts/kanban.js
+++ Login/scripts/kanban.js
@@ -2,23 +2,49 @@ const $modal = document.getElementById('modal');
 const $descriptionInput = document.getElementById('descricao');
 const $priorityInput = document.getElementById('prioridade');
 const $deadLineInput = document.getElementById('deadline');
+const $idInput = document.getElementById('idInput');    
 
 const $todoColumnBody = document.querySelector('#todoColumn .body');
 
 var todoList = []
 
-function openModal(){
+function openModal(id){
     $modal.style.display = "flex";
+
+    if(id){
+        const index = todoList.findIndex(function(task){
+            return task.id === id;
+        });
+
+        if (index !== -1) {
+            const task = todoList[index];
+
+            $idInput.value = task.id;
+            $descriptionInput.value = task.description;
+            $priorityInput.value = task.priority;
+            $deadLineInput.value = task.deadline;
+        } else {
+            console.error('Task not found');
+        }
+    }   
 }
 
 function closeModal(){
     $modal.style.display = "none";
+    $descriptionInput.value = "";
+    $priorityInput.value = "";
+    $deadLineInput.value = "";  
+}
+
+function generateUniqueId() {
+    return Math.floor(Math.random() * 10000);
 }
 
 function generateCards() {
     const todoListHtml = todoList.map(function(task) {
+        const formattedDeadline = moment(task.deadline).format('DD/MM/YYYY');
          return `
-            <div class="card">
+            <div class="card" ondblclick="openModal(${task.id})">
                 <div class="info">
                     <b>Descrição</b>
                     <span>${task.description}</span>
@@ -31,7 +57,7 @@ function generateCards() {
 
                 <div class="info">
                     <b>Deadline</b>
-                    <span>${task.deadline}</span>
+                    <span>${formattedDeadline}</span>
                 </div>
             </div>
         `;
@@ -42,13 +68,12 @@ function generateCards() {
 
 function createTask() {
     const newTask = {
+        id: generateUniqueId(),
         description: $descriptionInput.value,
         priority: $priorityInput.value,
         deadline: $deadLineInput.value,
-    }
-
+    };
     todoList.push(newTask);
-
-    closeModal();
     generateCards();
+    closeModal();
 }
